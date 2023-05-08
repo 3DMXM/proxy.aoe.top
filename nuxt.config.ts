a30a53c830b3780d81fddd54dd8b2f8bd58ec2ed
小莫 <xiaomo1433675239@outlook.com>
2023-05-08 18:01:54 +0800

    添加文件

---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    modules: [
+        '@nuxt-alt/proxy',
+    ],
+    proxy: {
+        proxies: {
+            '/shields': {
+                // target: 'https://localhost:5000',
+                target: 'https://img.shields.io',
+                changeOrigin: true,
+                rewrite: (path: any) => path.replace(/^\/shields/, '')
+            },
+        }
+    },
+})
